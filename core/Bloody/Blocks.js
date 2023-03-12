5a6dc2fee17e99f8c2f3986ad687ba7824fb6642
Artem0n4 <95755528+Artem0n4@users.noreply.github.com>
2023-03-12 19:43:13 +0300

    Add files via upload

---
+++ core/Bloody/Blocks.js
@@ -0,0 +1,29 @@
+﻿const BLOCK_TYPE_LIQUID = Block.createSpecialType({
+    solid: false,
+    renderlayer: 1,
+    explosionres: 10000
+});
+
+ LiquidRegistry.registerLiquid("bloody_liquid", "Blood", ["bloody_liquid_flow"]); 
+ 
+Block.createLiquidBlock("bloody_liquid", 
+{ 
+ name: "Blood", 
+ still: { 
+ texture: ["bloody_liquid_still", 0], 
+ id: "bloody_liquid_still",
+ }, 
+ flowing: { 
+ texture: ["bloody_liquid_flow", 0], 
+ id: "bloody_liquid_flowing",
+ }, 
+ bucket: { 
+ texture: { name: "bucket_blood", meta: 0 }, 
+ name: "Bucket of terrible blood",
+ id: "bucket_of_blood",
+ }, 
+}, BLOCK_TYPE_LIQUID);
+
+IDRegistry.genBlockID("bloody_block");
+Block.createBlock("bloody_block", [
+	{name: "Hardened blood", texture: [["hardened_blood", 0]], inCreative: true}]);
---
+++ core/Bloody/Items.js
@@ -0,0 +1,5 @@
+﻿
+﻿var textureid = FileTools.GetListOfFiles(__dir__+"assets/items-opaque/","png")
+﻿for(var i in textureid){
+﻿IDRegistry.genItemID(textureid[i]); 
+Item.createItem(textureid[i], textureid[i], {name: textureid[i], meta: 0}, {stack: 64});}
